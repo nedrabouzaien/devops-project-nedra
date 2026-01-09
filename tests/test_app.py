89a9552e8c2b1a65b4f219f6043d3804cb61a6b8
Nedra Bouzaien <nedrabouzaien9@gmail.com>
2026-01-09 23:20:28 +0100

    Add basic unit tests for Flask API

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,8 @@
-import json
+import sys, os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from app import app
+
+import json
 
 def test_health():
     client = app.test_client()
